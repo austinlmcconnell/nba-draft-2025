ff3f7b74b58ca75e152ae63773aa25796518832d
Austin McConnell <144247988+austinlmcconnell@users.noreply.github.com>
2025-06-25 15:18:36 -0400

    Update nba_draft_2025_app.py

--- nba_draft_2025_app.py
+++ nba_draft_2025_app.py
@@ -106,6 +106,14 @@ team_colors = {
 }
 
 def get_team_color(team):
+    team_colors = {
+        "Hawks": "#E03A3E", "Celtics": "#008248", "Nets": "#FFFFFF", "Hornets": "#1D1160", "Bulls": "#CE1141",
+        "Cavaliers": "#6F263D", "Mavericks": "#00538C", "Nuggets": "#0E2240", "Pistons": "#006BB6", "Warriors": "#FDB927",
+        "Rockets": "#CE1141", "Pacers": "#F6A01A", "Clippers": "#C8102E", "Lakers": "#FDB927", "Grizzlies": "#5D76A9",
+        "Heat": "#98002E", "Bucks": "#00471B", "Timberwolves": "#236192", "Pelicans": "#85714D", "Knicks": "#F58426",
+        "Thunder": "#007AC1", "Magic": "#0077C0", "76ers": "#006BB6", "Suns": "#E56020", "Trail Blazers": "#E03A3E",
+        "Kings": "#5A2D81", "Spurs": "#C4CED4", "Raptors": "#CE1141", "Jazz": "#002B5C", "Wizards": "#002B5C"
+    }
     return team_colors.get(team, "white")
 
 def get_grade_color(grade):
